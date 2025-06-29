30359b82b66f16e7e4efb2b578c2396ac5e6f32a
CodeAneeq <codewithaneeq.com>
2025-06-29 20:08:43 +0500

    Contact Changes

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,147 +1,3 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import Navbar from "../../components/navbar/Navbar";
-// import BasicLayout from "../../components/layout/basicLayout";
-// import contact from "../../assets/img/contactVectorRee.png";
-// import CategoryName from "../../components/category/CategoryName";
-// import styles from "./contact.module.scss";
-// import Input from "../../components/input/input";
-// import PrimaryBtn from "../../components/btns/primaryBtn";
-// import TextField from "../../components/input/TextField";
-// import hero from "../../assets/img/Hero.png";
-// import { FaUser } from "react-icons/fa";
-// import { MdEmail, MdOutlineMessage, MdOutlineSubject } from "react-icons/md";
-// import { motion } from "framer-motion";
-// import baseURL from "../../helpers/Constant";
-// import { FaMessage } from "react-icons/fa6";
-
-// const Contact = () => {
-//   const [email, setEmail] = useState("");
-//   const [subject, setSubject] = useState("");
-//   const [loading, setLoading] = useState(false);
-//   const [name, setName] = useState("");
-//   const [projectDesc, setProjectDesc] = useState("");
-//   const fadeInUp = {
-//     hidden: { opacity: 0, y: 20 },
-//     visible: { opacity: 1, y: 0 },
-//   };
-
-//   const sendMail = async (e) => {
-//     e.preventDefault();
-//     setLoading(true);
-//     try {
-//       const payload = { email, subject, name, projectDescription: projectDesc };
-//       const response = await axios.post(
-//         `${baseURL}/contact/api/send-mail`,
-//         payload
-//       );
-//       if (response?.data?.data?.status === "success") {
-//         console.log("Form Submitted");
-//       }
-//       setLoading(false);
-//     } catch (error) {
-//       console.log(error);
-//       setLoading(false);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <BasicLayout>
-//         <div className="container">
-//           <div className={styles.contact_page_container}>
-//             <CategoryName category={"Talk To Us"} />
-//             <p className={styles.contact_headin}>
-//               Feel free to send us your queries and details.
-//             </p>
-//             <div className={styles.contact_content}>
-//               <motion.figure
-//                 className={styles.avatar_container}
-//                 variants={fadeInUp}
-//                 transition={{ duration: 0.5 }}
-//                 initial="hidden"
-//                 whileInView="visible"
-//               >
-//                 <img
-//                   className={`${styles.avatar_img}`}
-//                   src={hero}
-//                   alt="contact vector"
-//                 />
-//                 <div>
-//               <h1>Have a project or startup idea in mind? Let’s build something amazing together.</h1>
-//                 </div>
-//               </motion.figure>
-
-//               <motion.form
-//                 className={styles.contact_inputs_div}
-//                 variants={fadeInUp}
-//                 transition={{ duration: 0.5 }}
-//                 initial="hidden"
-//                 whileInView="visible"
-//                 >
-//                 <div className={`${styles.input_one}`}>
-//                   <Input
-//                     value={name}
-//                     onChange={setName}
-//                     placeholder={"Name"}
-//                     icon={<FaUser />}
-//                     name={name}
-//                     required
-//                     />
-//                   <Input
-//                     value={subject}
-//                     onChange={setSubject}
-//                     placeholder={"Subject"}
-//                     icon={<MdOutlineSubject />}
-//                     name={subject}
-//                     required
-//                   />
-//                 </div>
-//                 <Input
-//                   value={email}
-//                   onChange={setEmail}
-//                   placeholder={"Email"}
-//                   icon={<MdEmail />}
-//                   name={email}
-//                   type="email"
-//                   required
-//                 />
-//                 <TextField
-//                   value={projectDesc}
-//                   onChange={setProjectDesc}
-//                   placeholder={"Project Description"}
-//                   name={projectDesc}
-//                   required
-//                 />
-//                 <div className={styles.btns}>
-//                   <div className={styles.btn_div}>
-//                     <PrimaryBtn
-//                       onClick={sendMail}
-//                       loading={loading ? true : false}
-//                       disabled={loading ? true : false}
-//                       type="submit"
-//                     >
-//                       Send Message <MdOutlineMessage />
-//                     </PrimaryBtn>
-//                   </div>
-//                 </div>
-
-//                 <p>
-//                   Note: We will contact you as soon as possible through your
-//                   email.
-//                 </p>
-//               </motion.form>
-//             </div>
-//           </div>
-//         </div>
-//       </BasicLayout>
-//     </div>
-//   );
-// };
-
-// export default Contact;
-
-
 import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
 import Navbar from "../../components/navbar/Navbar";
@@ -201,7 +57,7 @@ const Contact = () => {
       .then(
         (response) => {
           console.log("Email sent successfully!", response);
-          alert("Message Sent Successfully ✅");
+          setTxt("Message Sent Successfully ✅");
           setName("");
           setEmail("");
           setSubject("");
@@ -210,7 +66,7 @@ const Contact = () => {
         },
         (error) => {
           console.error("Email sending failed ❌", error);
-          alert("Failed to send message ❌");
+          setTxt("Failed to send message ❌");
           setLoading(false);
         }
       );
